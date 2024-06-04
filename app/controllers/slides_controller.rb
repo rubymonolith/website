331bdefe38a26325b53a4444b3ca42bc32db6124
Brad Gessler <bradgessler@gmail.com>
2024-06-03 17:02:35 -0700

    Add Tailwind syntax highlighting

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,4 +1,58 @@
+require "rouge"
+
 class SlidesController < ApplicationController
+  class RougeTailwindTheme < Rouge::Themes::Base16
+    name 'tailwind'
+
+    # Define the style mappings
+    style Comment,                class: "text-gray-500"
+    style Keyword,                class: "text-yellow-300"
+    style Literal::String,        class: "text-green-500"
+    style Literal::Number,        class: "text-purple-500"
+    style Text,                   class: "text-neutral-100"
+
+    style Generic::Output,        class: "text-neutral-100"
+    style Generic::Prompt,        class: "text-neutral-100"
+    style Generic::Error,         class: "text-red-500"
+    style Generic::Traceback,     class: "text-red-500"
+
+    style Name::Constant,         class: "text-purple-400"
+    style Name::Function,         class: "text-pink-300"
+    style Name::Builtin,          class: "text-neutral-100"
+    style Name::Class,            class: "text-pink-400"
+    style Name::Variable::Instance, class: "text-sky-300"
+
+    style Name::Decorator,        class: "text-blue-500"
+    style Name::Namespace,        class: "text-blue-400"
+    style Name::Attribute,        class: "text-blue-400"
+    style Name::Entity,           class: "text-blue-400"
+    style Name::Tag,              class: "text-blue-400"
+    style Name::Property,         class: "text-blue-400"
+    style Name::Attribute,        class: "text-blue-400"
+    style Name::Function,         class: "text-blue-400"
+
+    style Name::Variable::Global, class: "text-blue-400"
+
+    style Name::Variable::Class,  class: "text-blue-400"
+    style Name::Variable::Global, class: "text-blue-400"
+    style Name::Variable::Magic,  class: "text-blue-400"
+    style Name::Builtin::Pseudo,  class: "text-blue-400"
+    style Name::Builtin::Function, class: "text-blue-400"
+    style Name::Builtin::Variable, class: "text-blue-400"
+
+    # Add more styles as needed
+  end
+
+  class RougeTailwindHTMLFormatter < Rouge::Formatters::HTMLInline
+    def safe_span(tok, safe_val)
+      return safe_val if tok == Rouge::Token::Tokens::Text
+
+      class_name = @theme.style_for(tok).fetch(:class, nil)
+
+      "<span class=\"#{class_name}\">#{safe_val}</span>"
+    end
+  end
+
   class Presentation
     def self.register_layouts(layouts)
       include layouts
@@ -15,17 +69,18 @@ class SlidesController < ApplicationController
 
   module Layouts
     class Slide < ApplicationView
-      def initialize(title: nil, &block)
+      def initialize(title: nil, class: nil, &block)
         @title = title
+        @class = binding.local_variable_get(:class)
       end
 
       def around_template(&)
-        VStack("p-8 h-full", &)
+        VStack(tokens("p-12 h-full", @class), &)
       end
 
       def Code(language, source)
-        pre(class: "bg-gray-800 text-white rounded p-4") {
-          code { source }
+        pre(class: "bg-gray-800 text-white rounded-2xl p-4") {
+          code { format_code(source) }
         }
       end
 
@@ -36,13 +91,38 @@ class SlidesController < ApplicationController
       def VStack(classes, &)
         div(class: tokens("flex flex-col gap-8", classes), &)
       end
+
+      def Title(&)
+        h1(class: "font-bold text-4xl", &)
+      end
+
+      def Subtitle(&)
+        h1(class: "text-4xl", &)
+      end
+
+      def Markdown(source)
+        div(class: "prose") do
+          render inline: source, type: :md
+        end
+      end
+
+      private
+        def format_code(source)
+          formatter = RougeTailwindHTMLFormatter.new(RougeTailwindTheme.new)
+          lexer = Rouge::Lexers::Ruby.new
+          highlighted_code = formatter.format(lexer.lex(source))
+          unsafe_raw highlighted_code
+        end
     end
 
     class TitleSlide < Slide
+      def Title(&)
+        h1(class: "font-bold text-8xl", &)
+      end
+
       def around_template
         super do
           VStack "place-content-center h-full" do
-            h1(class: "font-bold text-6xl") { @title }
             yield
           end
         end
@@ -52,7 +132,7 @@ class SlidesController < ApplicationController
     class ContentSlide < Slide
       def around_template
         super do
-          h1(class: "font-bold text-3xl") { @title }
+          Title { @title }
           yield
         end
       end
@@ -64,15 +144,21 @@ class SlidesController < ApplicationController
 
     def slides
       [
-        TitleSlide(title: "Build Rails Applications with 100% Phlex"){
-          h2(class: "text-4xl") { "Phlex is a new way to build Rails applications." }
+        TitleSlide(
+          title: "Build Rails Applications with 100% Phlex 💪",
+          class: "bg-blue-500 text-white"
+        ){
+          Title { @title }
+          Subtitle { "Phlex is a new way to build Rails applications." }
         },
 
-        ContentSlide(title: "What is Phlex?"){
+        ContentSlide(
+          title: "What is Phlex?"
+        ){
           p { "Phlex is a plain 'ol Ruby object" }
           p { "It's a new way to build Rails applications. Here's what it looks like:" }
           Code :ruby, <<~RUBY
-            class MyComponent < Phlex::Component
+            class MyComponent < Phlex::HTML
               def initialize(name:)
                 @name = name
               end
@@ -84,10 +170,20 @@ class SlidesController < ApplicationController
           RUBY
         },
 
-        ContentSlide(title: "Why Phlex?"){
+        ContentSlide(
+          title: "Why Phlex?"
+        ){
           p { "Build EVERYTHING in Ruby" }
           p { "Simpler API than ViewComponents" }
           p { "Localize view state" }
+        },
+
+        ContentSlide(title: "Why Phlex?"){
+          Markdown <<~MARKDOWN
+
+          * Because its fun
+          * Because its super-de-dooper
+          MARKDOWN
         },
       ]
     end
@@ -101,7 +197,7 @@ class SlidesController < ApplicationController
     def view_template
       ol(class: "grid grid-cols-1 gap-12 p-12 max-w-screen-xl mx-auto") {
         @presentation.slides.each do |slide|
-          li(class: "w-full aspect-[16/9] border border-gray-300 rounded-lg p-4 shadow-xl") {
+          li(class: "w-full aspect-[16/9] border border-gray-300 rounded-lg overflow-hidden shadow-xl") {
             render slide
           }
         end
