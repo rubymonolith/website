4326e5221315d367fa6772dc960a8728cb11381c
Brad Gessler <bradgessler@gmail.com>
2025-10-21 21:24:53 -0700

    Add a gem redirect controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,27 @@ Rails.application.routes.draw do
     resources :phlex_slides
   end
 
+  resources :gems do
+    member do
+      get :homepage
+      get :documentation
+      get :docs, to: "gems#documentation"
+      get :changes
+      get :changelog, to: "gems#changes"
+      get :versions
+      get :source_code
+      get :source, to: "gems#source_code"
+      get :bug_tracker
+      get :bugs, to: "gems#bug_tracker"
+      get :issues, to: "gems#bug_tracker"
+      get :mailing_list
+      get :wiki
+      get :funding
+      get :project
+      get :gem
+    end
+  end
+
   sitepress_pages
   sitepress_root
 end
